709a1410cf1f4b10a889143424f49633d49f8e6c
Tomoya Watanabe <tomoya@ryogeisya.jp>
2020-04-01 17:51:29 +0900

    todoページを追加

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <form @submit.prevent="addTodo">
+    <input type="text" v-model="text" />
+    <button type="submit">追加</button>
+  </form>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Emit } from "vue-property-decorator";
+
+@Component
+export default class TodoItem extends Vue {
+  text = "";
+
+  @Emit("addTodo")
+  emitAddTodo(text: string): string {
+    return text;
+  }
+
+  addTodo() {
+    this.emitAddTodo(this.text);
+    this.text = "";
+  }
+
+}
+</script>
